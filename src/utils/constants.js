30f3261e5e2e3a1672e3809f0cda79362f0f97ae
joaquimoiio <joaquimpaes01@gmail.com>
2026-02-19 17:12:37 -0300

    Agora vai

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8080/api';
+export const API_BASE = 'https://posteback.onrender.com/api';
 
 export const METODOS_PAGAMENTO = [
   { value: 'PIX_JEFF',     label: 'Pix do Jeff' },
